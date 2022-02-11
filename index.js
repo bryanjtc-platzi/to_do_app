d7e49083209364720de2c522f929b0040e9cad27
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-02-11 17:26:45 -0500

    Fix express vulnerabilities Fixes #3

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const csrf = require("csurf");
+const helmet = require("helmet");
+const csrfProtection = csrf({ cookie: true });
+const app = express();
+
+app.use(helmet());
 
 // http
 // cliente <-> servidor
@@ -13,31 +19,35 @@ const db = {}
 
 // camel case
 function handleGetHelloWorld(req, res) {
-	console.log(req.propiedadArbitraria)
-	return res.json(db)
+  console.log(req.propiedadArbitraria);
+  return res.json(db);
 }
 
 function handlePostHelloWorld(req, res) {
-	const id = Math.random()
-	db[id] = req.body
-	return res.send('Hola desde POST')
+  const id = Math.random();
+  db[id] = req.body;
+  return res.send("Hola desde POST");
 }
 
 function testMiddleware1(req, res, next) {
-	console.log('HOLA DESDE EL MIDDLEWARE')
-	req.propiedadArbitraria = 100
-	next()
+  console.log("HOLA DESDE EL MIDDLEWARE");
+  req.propiedadArbitraria = 100;
+  next();
 }
 
 function testMiddleware2(req, res, next) {
-	console.log('HOLA ESTOY EN EL 2')
-	next()
+  console.log("HOLA ESTOY EN EL 2");
+  next();
 }
 
-app.use(bodyParser.json())
-app.use(testMiddleware1)
-app.use(testMiddleware2)
-app.route('/hello-world').get(handleGetHelloWorld).post(handlePostHelloWorld)
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(testMiddleware1);
+app.use(testMiddleware2);
+app
+  .route("/hello-world")
+  .get(csrfProtection, handleGetHelloWorld)
+  .post(handlePostHelloWorld);
 app.listen(3000, () => {
-	console.log('Servidor corriendo en puerto 3000...')
-})
+  console.log("Servidor corriendo en puerto 3000...");
+});
